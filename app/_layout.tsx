f32e66a66974f4642f158d6dc9ab809ba5e93c78
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-01-28 20:48:56 +0530

    Flash Screen Added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
+
+import React from "react";
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const _layout = () => {
+  return <Stack screenOptions={{ headerShown: false }}></Stack>;
+};
+
+export default _layout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,44 @@
-import { Text, View } from "react-native";
+import { View, Text ,Image} from 'react-native'
+import React, { useEffect } from 'react'
+import { StyleSheet } from 'react-native'
+import {colors} from '../constants/theme'
+import { useRouter } from 'expo-router'
 
-export default function Index() {
+
+const index = () => {
+  const router=useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/welcome')
+    }, 2000);
+  }, [])
+  
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={style.container}>
+      <Image
+      style={style.logo}
+        resizeMode='contain'
+        source={require('../assets/images/splashImage.png')}
+      />
     </View>
-  );
+  )
 }
+
+export default index
+
+const style=StyleSheet.create({
+container:{
+flex:1,
+justifyContent:'center',
+alignItems:'center',
+backgroundColor:colors.neutral900,
+
+},
+logo:{
+  height:'20%',
+  aspectRatio:1
+
+
+
+}
+})
