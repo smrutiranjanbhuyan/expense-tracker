85bbb2c62d66ee0449ee46b2d211802af8ff1ae0
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-01-29 20:57:22 +0530

    `Added Login Component`

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,69 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import BackButton from "@/components/BackButton";
+import Input from "@/components/Input";
+
+const login = () => {
+  return (
+    <ScreenWrapper>
+      <View style={styles.container}>
+        <BackButton iconSize={28} />
+        <View style={{ gap: 5, marginTop: spacingY._20 }}>
+          <Typo size={30} fontWeight={"800"}>
+            Hey,
+          </Typo>
+          <Typo size={30} fontWeight={"800"}>
+            Welcome Back
+          </Typo>
+        </View>
+
+        {/* {form} */}
+        <View style={styles.form}>
+          <Typo size={16} color={colors.textLight}>
+              Login now to track all your expenses
+          </Typo>
+       <Input/>
+
+        </View>
+      </View>
+    </ScreenWrapper>
+  );
+};
+
+export default login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: spacingY._30,
+    paddingHorizontal: spacingX._20,
+  },
+  welcomeText: {
+    fontSize: verticalScale(20),
+    fontWeight: "bold",
+    color: colors.text,
+  },
+  form: {
+    gap: spacingY._20,
+  },
+  forgotPassword: {
+    textAlign: "right",
+    fontWeight: "500",
+    color: colors.text,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  footerText: {
+    textAlign: "center",
+    color: colors.text,
+    fontSize: verticalScale(15),
+  },
+});
