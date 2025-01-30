b7d48545286226cbd607749bbc2145a0f1bd48ca
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-01-30 09:25:43 +0530

    `feat: added auth context and provider to register page and layout`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
 
 import React from "react";
 import { Stack } from "expo-router";
+import { AuthProvider } from "@/contexts/authContext";
 
-const _layout = () => {
+const StackLayout = () => {
   return <Stack screenOptions={{ headerShown: false }}></Stack>;
 };
 
-export default _layout;
+export default function Rootlayout(){
+  <AuthProvider>
+     <StackLayout/>
+  </AuthProvider>
+};
+
