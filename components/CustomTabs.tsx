7be7ffc100913f2deeb5824c9b1806e4a3f30e59
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-01-30 21:13:24 +0530

    `feat: create custom bottom tab bar UI
    - Implemented a custom bottom tab bar for navigation
    - Added tab icons using Phosphor React Native icons
    - Ensured proper focus state handling for active/inactive tabs
    - Optimized styling and platform-specific adjustments
    `

---
+++ components/CustomTabs.tsx
@@ -0,0 +1,105 @@
+import { View, Platform, TouchableOpacity, StyleSheet } from "react-native";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import { colors, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import * as Icons from "phosphor-react-native";
+
+export default function CustomTabs({ state, descriptors, navigation }: BottomTabBarProps) {
+  const tabBarIcons: any = {
+    index: (isFocused: boolean) => (
+      <Icons.House
+        size={verticalScale(30)}
+        weight={isFocused ? "fill" : "regular"}
+        color={isFocused ? colors.primary : colors.neutral400}
+      />
+    ),
+    stastics: (isFocused: boolean) => (
+      <Icons.ChartBar
+        size={verticalScale(30)}
+        weight={isFocused ? "fill" : "regular"}
+        color={isFocused ? colors.primary : colors.neutral400}
+      />
+    ),
+    wallet: (isFocused: boolean) => (
+      <Icons.Wallet
+        size={verticalScale(30)}
+        weight={isFocused ? "fill" : "regular"}
+        color={isFocused ? colors.primary : colors.neutral400}
+      />
+    ),
+    profile: (isFocused: boolean) => (
+      <Icons.User
+        size={verticalScale(30)}
+        weight={isFocused ? "fill" : "regular"}
+        color={isFocused ? colors.primary : colors.neutral400}
+      />
+    ),
+  };
+
+  return (
+    <View style={styles.tabbar}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label: string =
+          options.tabBarLabel !== undefined
+            ? (options.tabBarLabel as string)
+            : options.title !== undefined
+            ? (options.title as string)
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            key={route.key}
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarButtonTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={styles.tabbarItem}
+          >
+            {tabBarIcons[route.name] && tabBarIcons[route.name](isFocused)}
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabbar: {
+    flexDirection: "row",
+    width: "100%",
+    height: Platform.OS === "ios" ? verticalScale(73) : verticalScale(60),
+    backgroundColor: colors.neutral800,
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderTopColor: colors.neutral700,
+    borderTopWidth: 1,
+  },
+  tabbarItem: {
+    marginBottom: Platform.OS === "ios" ? verticalScale(10) : verticalScale(5),
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
