7292246e52b57b2258d2fa381b12b576cc700a8c
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-02-01 00:50:43 +0530

    `Added Firebase configuration details`

--- config/firebase.ts
+++ config/firebase.ts
@@ -8,7 +8,12 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
+  apiKey: "AIzaSyACYpR7XI9eHf9-2ES1cPZcTKvINTiyr60",
+  authDomain: "expense-tracker-74041.firebaseapp.com",
+  projectId: "expense-tracker-74041",
+  storageBucket: "expense-tracker-74041.firebasestorage.app",
+  messagingSenderId: "242274059526",
+  appId: "1:242274059526:web:a183643757c6fa319f0a87",
 };
 
 // Initialize Firebase
