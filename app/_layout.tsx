55ddd3e60888066d029fbfcf125798bc15d3a477
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-02-02 00:20:39 +0530

    `Added StatusBar and theme colors to Rootlayout`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Stack } from "expo-router";
 import { AuthProvider } from "@/contexts/authContext";
-
+import StatusBar  from "@/components/StatusBar";
+import { colors } from "@/constants/theme";
 const StackLayout = () => {
   return <Stack screenOptions={{ headerShown: false }} >
     <Stack.Screen
@@ -19,6 +20,7 @@ const StackLayout = () => {
 export default function Rootlayout() {
   return (
     <AuthProvider>
+      <StatusBar  backgroundColor={colors.neutral900} barStyle="light-content" />
       <StackLayout />
     </AuthProvider>
   );
