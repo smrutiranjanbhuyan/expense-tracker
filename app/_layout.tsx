f507ee5658080aa553182bb9b34a4495211c8366
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-02-02 09:51:32 +0530

    `Update app/_layout.tsx with new Stack screen options and StatusBar import`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,33 @@
 import React from "react";
 import { Stack } from "expo-router";
 import { AuthProvider } from "@/contexts/authContext";
-import StatusBar  from "@/components/StatusBar";
+import StatusBar from "@/components/StatusBar";
 import { colors } from "@/constants/theme";
 const StackLayout = () => {
-  return <Stack screenOptions={{ headerShown: false }} >
-    <Stack.Screen
-    name="(models)/profileModel"
-    options={
-      {
-        presentation :'modal',
-         animation: 'slide_from_bottom',
-      }
-    }
-    />
-  </Stack>;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        animation: "ios_from_right",
+        gestureEnabled: true,
+        animationTypeForReplace: "pop",
+      }}
+    >
+      <Stack.Screen
+        name="(models)/profileModel"
+        options={{
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      />
+    </Stack>
+  );
 };
 
 export default function Rootlayout() {
   return (
     <AuthProvider>
-      <StatusBar  backgroundColor={colors.neutral900} barStyle="light-content" />
+      <StatusBar backgroundColor={colors.neutral900} barStyle="light-content" />
       <StackLayout />
     </AuthProvider>
   );
