d2875a42efc62938400d7f2d9fc89966920ecf6e
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-02-10 21:47:42 +0530

    feat: enhance TransactionList to display income categories and format transaction dates

--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -6,8 +6,9 @@ import { colors, radius, spacingY } from "@/constants/theme";
 import Typo from "./Typo";
 import { FlashList } from "@shopify/flash-list";
 import Loading from "./Loading";
-import { expenseCategories } from "@/constants/data";
+import { expenseCategories, incomeCategory } from "@/constants/data";
 import Animated, { FadeIn, FadeInDown } from "react-native-reanimated";
+import { Timestamp } from "firebase/firestore";
 
 const TransactionList = ({
   data,
@@ -62,12 +63,23 @@ const TransactionItem = ({
   index,
   handleClick,
 }: TransactionItemProps) => {
-  let category = expenseCategories["rent"];
+  let category =
+    item?.type == "income" ? incomeCategory : expenseCategories[item.category!];
   const IconComponent = category.icon;
-
+  const date = (item.date as Timestamp)?.toDate()?.toLocaleDateString("en-GB", { 
+    day: "numeric", 
+    month: "short", 
+    year: "numeric"
+  });
+  
   return (
-    <Animated.View entering={FadeInDown.delay(index * 70).springify().damping(14)} style={styles.row}>
-      <TouchableOpacity style={styles.row} onPress={()=>handleClick}>
+    <Animated.View
+      entering={FadeInDown.delay(index * 70)
+        .springify()
+        .damping(14)}
+      style={styles.row}
+    >
+      <TouchableOpacity style={styles.row} onPress={() => handleClick}>
         <View style={[styles.icon, { backgroundColor: category.bgColor }]}>
           {IconComponent && (
             <IconComponent
@@ -89,11 +101,16 @@ const TransactionItem = ({
         </View>
 
         <View style={styles.amountDate}>
-          <Typo  fontWeight={"500"} color={colors.primary}>
-            +100
+          <Typo
+            fontWeight={"500"}
+            color={item?.type == "income" ? colors.primary : colors.rose}
+          >
+            {item?.type == "income" ? "+ ₹" + item?.amount : "- ₹" + item?.amount}
           </Typo>
           <Typo size={13} color={colors.neutral400}>
-            12:00 PM
+           {
+            date
+           }
           </Typo>
         </View>
       </TouchableOpacity>
