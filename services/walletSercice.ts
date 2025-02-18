11a770d61c84cb593c19caca57f2ac54da3055cb
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-02-18 23:39:07 +0530

    feat: implement wallet deletion with cascading transaction removal

--- services/walletSercice.ts
+++ services/walletSercice.ts
@@ -6,6 +6,10 @@ import {
   setDoc,
   serverTimestamp,
   deleteDoc,
+  query,
+  where,
+  getDocs,
+  writeBatch,
 } from "firebase/firestore";
 import { firestore } from "@/config/firebase";
 
@@ -17,9 +21,9 @@ export const createOrUpdateWallet = async (
       return { success: false, message: "Invalid wallet data" };
     }
 
-  let walletToSave = {
-  ...walletData,
-};
+    let walletToSave = {
+      ...walletData,
+    };
     // Handle Image Upload
     if (walletData.image?.uri) {
       const imageUploadRes = await uploadFileToCloudinary(
@@ -47,15 +51,48 @@ export const createOrUpdateWallet = async (
   } catch (error: any) {
     console.error("Error creating or updating wallet", error);
     return { success: false, message: error?.message || "An error occurred" };
-  }
+  }     
 };
 
 export const deleteWallet = async (walletId: string): Promise<ResponseType> => {
   try {
     const walletRef = doc(firestore, "wallets", walletId);
     await deleteDoc(walletRef);
-    // Delete wallet from storage
-    return { success: true };
+    deleteTransactionByWalletId(walletId);
+
+    return { success: true, message: "Wallet deleted successfully" };
+  } catch (error: any) {
+    console.error("Error deleting wallet", error);
+    return { success: false, message: error?.message || "An error occurred" };
+  }
+};
+
+export const deleteTransactionByWalletId = async (
+  walletId: string
+): Promise<ResponseType> => {
+  try {
+    let hasMoreTransaction = true;
+
+    while (hasMoreTransaction) {
+      const transactionQuery = query(
+        collection(firestore, "transactions"),
+        where("walletId", "==", walletId)
+      );
+      const transactionSnapshot = await getDocs(transactionQuery);
+      if (transactionSnapshot.size == 0) {
+        hasMoreTransaction = false;
+        break;
+      }
+
+      const batch = writeBatch(firestore);
+
+      transactionSnapshot.forEach((transactionDoc) => {
+        batch.delete(transactionDoc.ref);
+      });
+      await batch.commit();
+    }
+
+    return { success: true, message: "All transactions deleted successfully" };
   } catch (error: any) {
     console.error("Error deleting wallet", error);
     return { success: false, message: error?.message || "An error occurred" };
