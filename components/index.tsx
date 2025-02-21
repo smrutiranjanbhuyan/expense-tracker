78ad41537f58b3f40ea74958410cbab9889a835d
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-02-21 13:00:43 +0530

    feat: add index file to export common components for easier imports

---
+++ components/index.tsx
@@ -0,0 +1,31 @@
+import BackButton from "./BackButton";
+import Button from "./Button";
+import CustomTabs from "./CustomTabs";
+import Header from "./Header";
+import HomeCard from "./HomeCard";
+import ImageUpload from "./ImageUpload";
+import Input from "./Input";
+import Loading from "./Loading";
+import ModelWrapper from "./ModelWrapper";
+import ScreenWrapper from "./ScreenWrapper";
+import TransactionList from "./TransactionList";
+import Typo from "./Typo";
+import WalletListItem from "./WalletListItem";
+import CustomStatusBar from "./StatusBar";
+
+export default {
+  BackButton,
+  Button,
+  CustomTabs,
+  Header,
+  HomeCard,
+  ImageUpload,
+  Input,
+  Loading,
+  ModelWrapper,
+  ScreenWrapper,
+  CustomStatusBar,
+  TransactionList,
+  Typo,
+  WalletListItem,
+};
