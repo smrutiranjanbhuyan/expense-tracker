9a6505889f93ef1e1a1bdd014a45c00d5c0c9d62
Smruti Ranjan Bhuyan <smruti452004@gmail.com>
2025-02-21 13:10:11 +0530

    refactor: simplify component imports in welcome file for better organization

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,8 @@
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import React from "react";
-import ScreenWrapper from "@/components/ScreenWrapper";
-import Typo from "@/components/Typo";
+import {ScreenWrapper,Typo,Button} from '@/components'
 import { colors, spacingX, spacingY } from "@/constants/theme";
 import { verticalScale } from "@/utils/styling";
-import Button from "@/components/Button";
 import Animated, { FadeIn, FadeInDown } from "react-native-reanimated";
 import { useRouter } from "expo-router";
 const Welcome = () => {
